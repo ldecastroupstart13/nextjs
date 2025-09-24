e3a2bfc577d7652bcab44da90802e438ba366016
ldecastroupstart13 <ldecastro@upstart13.com>
2025-09-24 13:43:34 -0300

    Create index.ts

---
+++ components/menus/index.ts
@@ -0,0 +1,3 @@
+export { default as HorizontalMenuExpectant } from "./HorizontalMenuExpectant"
+export { default as HorizontalMenuGladney } from "./HorizontalMenuGladney"
+export { default as HorizontalMenuTraffic } from "./HorizontalMenuTraffic"
