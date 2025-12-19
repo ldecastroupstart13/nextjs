abb463b306b4f8dbd568624c1d6cf99cce4824a9
ldecastroupstart13 <ldecastro@upstart13.com>
2025-12-19 10:26:43 -0700

    Add new allowed email to access control

--- middleware.ts
+++ middleware.ts
@@ -21,7 +21,7 @@ export default withAuth(
 
     // 🔐 Verifica se o usuário tem acesso (email específico ou domínio permitido)
     const email = req.nextauth.token.email as string
-    const ALLOWED_EMAILS = ["leonardo.decastro.brazil@gmail.com", "oakley.jones@gladney.org", "chandler.markwardt@gladney.org", "ashley.whiteside@gladney.org", "oakley@adoption.com"]
+    const ALLOWED_EMAILS = ["leonardo.decastro.brazil@gmail.com",  "jairosm88@gmail.com", "rjohnson@mojomarketingconsulting.com", "oakley.jones@gladney.org", "chandler.markwardt@gladney.org", "ashley.whiteside@gladney.org", "oakley@adoption.com"]
     const ALLOWED_DOMAIN = "@upstart13.com"
 
     const hasAccess =
